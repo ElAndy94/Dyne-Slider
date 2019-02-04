a1b2f646405ce793fa1fa90136c83410c3cba015
Andrew <andrewpeliza@hotmail.com>
2019-02-04 12:51:12 +0000

    Updates

---
+++ precache-manifest.f47cef88bdb75db93dbe71348302ddfa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "129185ed200050f18dad",
+    "url": "/Dyne-Slider/static/css/main.aa7993d4.chunk.css"
+  },
+  {
+    "revision": "129185ed200050f18dad",
+    "url": "/Dyne-Slider/static/js/main.129185ed.chunk.js"
+  },
+  {
+    "revision": "f22fde3f0ca9fa1ca6b1",
+    "url": "/Dyne-Slider/static/js/1.f22fde3f.chunk.js"
+  },
+  {
+    "revision": "f6c717082b0ceea67329",
+    "url": "/Dyne-Slider/static/js/runtime~main.f6c71708.js"
+  },
+  {
+    "revision": "c89ddaa34101b6cbfde4810c6ec0ce50",
+    "url": "/Dyne-Slider/static/media/tile1.c89ddaa3.png"
+  },
+  {
+    "revision": "815b26f60a39b6a819e5c21df8a768da",
+    "url": "/Dyne-Slider/static/media/tile2.815b26f6.png"
+  },
+  {
+    "revision": "6550f820bb00061b49ba5cdd0417620b",
+    "url": "/Dyne-Slider/static/media/tile3.6550f820.png"
+  },
+  {
+    "revision": "a0e5c1bd83ebf11df732b608697e5d1a",
+    "url": "/Dyne-Slider/index.html"
+  }
+];
